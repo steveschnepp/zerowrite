3e7dd8d3a2e4a40aaf942476af327a90f7271498
Steve Schnepp <steve.schnepp@pwkf.org>
2021-02-04 22:19:17 +0100

    zerowrite: implement nb

--- zerowrite.c
+++ zerowrite.c
@@ -17,8 +17,8 @@ int main(int argc, const char* argv[]) {
     }
 
     const char *filename = argv[1];
-    size_t buffer_size = atol(argv[2]);
-    size_t nb = 1;
+    size_t buffer_size = atoll(argv[2]);
+    long long nb = 1;
 
     if (argc > 3) {
         nb = atoll(argv[3]);
@@ -28,11 +28,17 @@ int main(int argc, const char* argv[]) {
     int perms =  S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
     int fd = open(argv[1], flags, perms);
 
-    ftruncate(fd, buffer_size);
 
-    char *fd_content = mmap(NULL, buffer_size, PROT_WRITE, MAP_SHARED, fd, 0);
-    memset(fd_content, 0, buffer_size);
-    munmap(fd_content, buffer_size);
+    size_t offset = 0;
+    for (long long i = 0; i < nb; i ++) {
+        ftruncate(fd, offset + buffer_size);
+
+        char *fd_content = mmap(NULL, buffer_size, PROT_WRITE, MAP_SHARED, fd, offset);
+        memset(fd_content, offset, buffer_size);
+        munmap(fd_content, buffer_size);
+
+        offset += buffer_size;
+    }
 
     close(fd);
 }
